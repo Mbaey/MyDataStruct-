640416f5d17909b0bbe0e420115f0af7caa72f7d
Mbaey <1092460929@qq.com>
2017-08-30 18:40:41 +0800

    看了STL list代码才发觉只不过换个语言又写一遍这些结构很傻。iterator真的很好用，值得学习

--- homework/ArrayList.cpp
+++ homework/ArrayList.cpp
@@ -10,128 +10,122 @@ using namespace std;
 #define  LIST_INCREMENT	10
 typedef  int	Status;
 typedef  int	ElemType;
-class SqList
+//c语言版本
+//Status Init_SqList( ArrayList *L );
+//Status Push_SqList( ArrayList *L, ElemType e);
+//Status Insert_SqList( ArrayList *L, int i, ElemType e );
+//ElemType  Delete_SqList( ArrayList *L, int i );
+class ArrayList
 {
 public:
-	ElemType	*Elem_array;
-	int		length;
+    //如果直接用vector就方便很多了=。=
+	ElemType	*array;
+	int		length=0;
     int Max_size;
 
-    SqList();
-    ~SqList();
+    ArrayList();
+    ~ArrayList();
     Status push(ElemType e);
     ElemType get(int index);
     void display();
-    ElemType pop();
+
+    Status erase(int index);
+    Status insert(int index, ElemType e);
+    Status update(int index, ElemType e);
+
 };
 
-SqList::SqList(){
+ArrayList::ArrayList(){
 //    cout << "constructor" << endl;
 //    Elem_array = ( ElemType * ) malloc( MAX_SIZE * sizeof(ElemType) );
-    Elem_array = new ElemType[MAX_SIZE];
-	if ( !Elem_array )	exit(ERROR);
+    array = new ElemType[MAX_SIZE];
+	if ( !array )	exit(ERROR);
 	else { length = 0; Max_size = MAX_SIZE;}
 }
-SqList::~SqList(){
+ArrayList::~ArrayList(){
     cout << "destructor" << endl;
 //    free(Elem_array);
-    delete [] Elem_array;
+    delete [] array;
 }
 
-Status SqList::push(ElemType e){
+Status ArrayList::push(ElemType e){
     if(length >= MAX_SIZE){
         //先复制。再拷贝回去
         //还是malloc 与relloc方便
-        cout << "//先复制。再拷贝回去" << endl;
+//        cout << "//先复制。再拷贝回去" << endl;
         ElemType temp[Max_size];
-        memcpy(temp, Elem_array, sizeof(temp));
+        memcpy(temp, array, sizeof(temp));
         Max_size += LIST_INCREMENT;
-        Elem_array = new ElemType[Max_size];
-        if(!Elem_array) return ERROR;
-        memcpy(Elem_array, temp, sizeof(temp));
+        delete [] array;
+        array = new ElemType[Max_size];
+        if(!array) return ERROR;
+        memcpy(array, temp, sizeof(temp));
     }
-    Elem_array[length++] = e;
+    array[length++] = e;
     return OK;
 }
-ElemType SqList::get(int index){
+ElemType ArrayList::get(int index){
     if(index < length){
-        return Elem_array[index];
+        return array[index];
     }else{
         cout << "下标越界" << endl;
         return ERROR;
     }
 }
-void SqList::display(){
+void ArrayList::display(){
     int i;
-    for(i=0; i<length-1; i++) cout << Elem_array[i] << " ";
-    cout << Elem_array[length-1] << endl;
+    for(i=0; i<length-1; i++) cout << array[i] << " ";
+    cout << array[length-1] << endl;
 }
 
-Status Init_SqList( SqList *L )
-{
-//    cout << "init" << endl;
-	L->Elem_array = ( ElemType * ) malloc( MAX_SIZE * sizeof(ElemType) );
-	if ( !L->Elem_array )
-		return(ERROR);
-	else { L->length = 0;    return(OK);  }
-}
-
-Status Push_SqList( SqList *L, ElemType e)
-{
-    if ( L->length >= MAX_SIZE )
-	{
-		printf( "线性表溢出!\n" );
-		return(ERROR);
-	}
-
-}
-
-
-Status Insert_SqList( SqList *L, int i, ElemType e )
-{
-	int j;
-	if ( i < 0 || i > L->length - 1 )
-		return(ERROR);
-	if ( L->length >= MAX_SIZE )
-	{
-		printf( "线性表溢出!\n" );
-		return(ERROR);
-	}
-
-    for ( j = L->length - 1; j >= i - 1; --j )
-    {
-        L->Elem_array[j + 1] = L->Elem_array[j];
+Status ArrayList::erase(int index){
+    if(index < length){
+        for(int i=index; i<length-1; i++){
+            array[i] = array[i+1];
+        }
+        array[length--]=0;
+        return OK;
+    }else{
+        cout << "下标越界" << endl;
+        return ERROR;
     }
+}
+//不能插到最后一个位置
+Status ArrayList::insert(int index, ElemType e){
 
-/*  i-1位置以后的所有结点后移  */
-	L->Elem_array[i - 1] = e; /*  在i-1位置插入结点  */
-	L->length++;
-	return(OK);
+    if(index < length && length + 1<MAX_SIZE){
+        length++;
+        for(int i=length-1; i>index; i--){
+            array[i] = array[i-1];
+        }
+        array[index] = e;
+
+        return OK;
+    }else{
+        cout << "下标越界" << endl;
+        return ERROR;
+    }
 }
 
-
-ElemType  Delete_SqList( SqList *L, int i )
-{
-	int k;   ElemType x;
-	if ( L->length == 0 )
-	{
-		printf( "线性表L为空!\n" ); return(ERROR);
-	}else if ( i < 1 || i > L->length )
-	{
-		printf( "要删除的数据元素不存在!\n" );
-		return(ERROR);
-	}else { x = L->Elem_array[i - 1]; /*保存结点的值*/
-		for ( k = i; k < L->length; k++ )
-			L->Elem_array[k - 1] = L->Elem_array[k];
-		/*  i位置以后的所有结点前移  */
-		L->length--;  return(x); }
+Status ArrayList::update(int index, ElemType e){
+    if(index < length){
+        array[index] = e;
+        return OK;
+    }else{
+        cout << "下标越界" << endl;
+        return ERROR;
+    }
 }
 
-void union_Array(SqList *L, int a[], int b[], int aLen, int bLen){
+void union_Array(ArrayList *L, int a[], int b[], int aLen, int bLen){
     int n = aLen + bLen;
     int i, c=0, d=0;
     for(i=0; i<n; i++){
-        if(a[c]>b[d]){
+        if(c==aLen){
+            (*L).push(b[d++]);
+        }else if(d==bLen){
+            (*L).push(a[c++]);
+        }else if(a[c]>b[d]){
             (*L).push(b[d++]);
         }else{
             (*L).push(a[c++]);
@@ -142,23 +136,81 @@ void union_Array(SqList *L, int a[], int b[], int aLen, int bLen){
 
 int main()
 {
-/*	freopen("1.txt", "r", stdin); */
-    SqList myList;
-
-//    myList.push(10);
-//    cout << myList.get(0) << endl;
-//    myList.push(100);
-//    cout << myList.get(0) << endl;
-//    cout << myList.get(1) << endl;
+    ArrayList myList;
 
     int a[] = {1,3,5,7,9};
     int b[] = {0,2,4,6,8};
     union_Array(&myList, a, b, 5, 5);
-    myList.push(1);
-    myList.push(1);
-    myList.push(1);
+    myList.push(10);
+    myList.push(100);
+    myList.push(1000);
+    myList.erase(0);
+    myList.insert(1, 888);
+    myList.update(2, 666);
     myList.display();
 	return(0);
 }
+
+
+//
+//Status Init_SqList( ArrayList *L )
+//{
+////    cout << "init" << endl;
+//	L->array = ( ElemType * ) malloc( MAX_SIZE * sizeof(ElemType) );
+//	if ( !L->array )
+//		return(ERROR);
+//	else { L->length = 0;    return(OK);  }
+//}
+//
+//Status Push_SqList( ArrayList *L, ElemType e)
+//{
+//    if ( L->length >= MAX_SIZE )
+//	{
+//		printf( "线性表溢出!\n" );
+//		return(ERROR);
+//	}
+//
+//}
+//
+//
+//Status Insert_SqList( ArrayList *L, int i, ElemType e )
+//{
+//	int j;
+//	if ( i < 0 || i > L->length - 1 )
+//		return(ERROR);
+//	if ( L->length >= MAX_SIZE )
+//	{
+//		printf( "线性表溢出!\n" );
+//		return(ERROR);
+//	}
+//
+//    for ( j = L->length - 1; j >= i - 1; --j )
+//    {
+//        L->array[j + 1] = L->array[j];
+//    }
+//
+///*  i-1位置以后的所有结点后移  */
+//	L->array[i - 1] = e; /*  在i-1位置插入结点  */
+//	L->length++;
+//	return(OK);
+//}
+//
+//
+//ElemType  Delete_SqList( ArrayList *L, int i )
+//{
+//	int k;   ElemType x;
+//	if ( L->length == 0 )
+//	{
+//		printf( "线性表L为空!\n" ); return(ERROR);
+//	}else if ( i < 1 || i > L->length )
+//	{
+//		printf( "要删除的数据元素不存在!\n" );
+//		return(ERROR);
+//	}else { x = L->array[i - 1]; /*保存结点的值*/
+//		for ( k = i; k < L->length; k++ )
+//			L->array[k - 1] = L->array[k];
+//		/*  i位置以后的所有结点前移  */
+//		L->length--;  return(x); }
+//}
 
 
--- homework/LinkedList.cpp
+++ homework/LinkedList.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 #define ERROR -1
-#define SUCCESS 1
+#define OK 1
 
 
 template <class T>
@@ -14,23 +14,24 @@ struct Node{
     public:
         T data=0;
         Node* next=NULL;
+        Node* prev=NULL;
 
     Node<T>()=default;
     Node<T>(T d): data(d){};
+    Node<T>(T d, Node* p, Node* n): data(d), prev(p), next(n){};
 //    Node(T d);
     ~Node();
 };
 
 template <class T> Node<T>::~Node(){
     cout << "data:"<< data <<"   next:"<< next << "xi gou han shu" << endl;
-    free(next);
 }
 
 template <class T>
 class LinkedList
 {
     public:
-        Node<T> head, *tail=NULL;
+        Node<T> *head=NULL, *tail=NULL;
         int len=0;
     public:
         LinkedList();
@@ -39,48 +40,65 @@ class LinkedList
         void push_back(T d);
         void push_head(T d);
         void display();
-        T searchByIndex(int index);
-        int deleteByIndex(int index);
+        T at(int index);
+        int erase(int index);
+        int update(int index, T d);
 
 };
 
-template <class T>  LinkedList<T>::LinkedList(){}
+template <class T>  LinkedList<T>::LinkedList(){
+    head = new Node<T>();
+    tail = new Node<T>();
+    head->next = tail;
+    tail->prev = head;
+    len=0;
+}
 
-template <class T>  LinkedList<T>::~LinkedList(){ delete tail;}
+template <class T>  LinkedList<T>::~LinkedList(){
+    Node<T> *p=head, *pre=NULL;
+    while(p!=NULL){
+        pre = p;
+        p = p->next;
+        delete pre;
+    }
+//如果没写迭代工具的话就只能这样实现了
+}
 
 template <class T> void LinkedList<T>::push_back(T d){
-    Node<T> *trueTail = new Node<T>(d);
+
     len++;
     if(len==1){
-        head.next = trueTail;
-        tail = trueTail;
+        tail->data = d;
     }else{
+        Node<T> *trueTail = new Node<T>(d, tail, NULL);
         tail->next = trueTail;
         tail = trueTail;
     }
 }
 
 template <class T> void LinkedList<T>::push_head(T d){
-    Node<T> *trueHead = new Node<T>(d);
     len++;
     if(len==1){
-        tail = trueHead;
+        tail->data = d;
     }else{
-        trueHead->next=head.next;
-        head.next= trueHead;
+        Node<T> *trueHead = new Node<T>(d, head, head->next);
+        head->next= trueHead;
+        (*trueHead->next).prev = trueHead;
+        return;
     }
 }
 
 template <class T> void LinkedList<T>::display(){
 
-    for(Node<T> *p = head.next; p->next != NULL; p = p->next){
+    for(Node<T> *p = head->next; p != NULL && p->next != NULL; p = p->next){
         cout << p->data << " ";
     }
-    cout << tail->data << endl;
+    if(len != 0)
+        cout << tail->data << endl;
 }
-template <class T> T LinkedList<T>::searchByIndex(int index){
+template <class T> T LinkedList<T>::at(int index){
     if(index <= len - 1){
-        Node<T> *p = head.next;
+        Node<T> *p = head->next;
         for(int i=0; i<index ;i++){
             p = p->next;
         }
@@ -88,20 +106,40 @@ template <class T> T LinkedList<T>::searchByIndex(int index){
     }
     return ERROR;
 }
-template <class T> int LinkedList<T>::deleteByIndex(int index){
+template <class T> int LinkedList<T>::update(int index, T d){
     if(index <= len - 1){
-        Node<T> *p = head.next;
-        Node<T> *pre = head.next;
+        Node<T> *p = head->next;
         for(int i=0; i<index ;i++){
-            pre = p;
             p = p->next;
         }
-        pre->next = p->next;
-//        delete p;
+        p->data = d;
+        return OK;
+    }
+    return ERROR;
+}
+template <class T> int LinkedList<T>::erase(int index){
+    if(index < len - 1){
+        Node<T> *p = head->next;
+
+        for(int i=0; i<index ;i++){
+            p = p->next;
+        }
+        (*p->prev).next = p->next;
+        (*p->next).prev = (p->prev);
+
         len--;
-        if(index == len-1)
-            tail = p->next;
-        return SUCCESS;
+        delete p;
+
+
+        return OK;
+    }else if(index == len - 1){
+        (*tail->prev).next = NULL;
+        tail = tail->prev;
+
+        len--;
+        delete tail->next;
+
+        return OK;
     }
     return ERROR;
 }
@@ -114,12 +152,13 @@ int main()
     myList.push_back(10);
     myList.push_back(100);
     myList.push_back(1000);
-    myList.push_head(0);
+    myList.push_head(88);
 
 
     myList.display();
-    cout << myList.searchByIndex(2)<< endl;
-    myList.deleteByIndex(2);
+    myList.update(2, 666);
+    cout << myList.at(3)<< endl;
+    myList.erase(1);
     cout << myList.len<< endl;
     myList.display();
     return 0;
